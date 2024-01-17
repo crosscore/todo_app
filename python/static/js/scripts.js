2588ee0c1f898904339efa98215d1253440ec90c
yuu <crosscore.g@gmail.com>
2024-01-17 12:00:12 +0900

    Adjust list up/down animation

--- python/static/js/scripts.js
+++ python/static/js/scripts.js
@@ -23,15 +23,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function moveItem(element, direction) {
     var id = element.getAttribute('data-id');
+    var li = element.closest('li');
+    var nextLi = direction === 'up' ? li.previousElementSibling : li.nextElementSibling;
+
+    if (nextLi) {
+        li.classList.add(direction === 'up' ? 'slide-fade-up' : 'slide-fade-down');
+        nextLi.classList.add(direction === 'up' ? 'slide-fade-down' : 'slide-fade-up');
+
+        setTimeout(function() {
+            // アニメーション後にDOMの更新を行う
+            if (direction === 'up') {
+                li.parentNode.insertBefore(li, nextLi);
+            } else {
+                li.parentNode.insertBefore(nextLi, li);
+            }
+
+            // アニメーションクラスの削除
+            li.classList.remove('slide-fade-up', 'slide-fade-down');
+            nextLi.classList.remove('slide-fade-up', 'slide-fade-down');
+
+            // 完全なアニメーションの完了を待つ
+            setTimeout(function() {
+                location.reload();
+            }, 300);
+        }, 250); // アニメーション時間
+    }
+
     fetch(`/move/${id}/${direction}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     }).then(response => {
-        if (response.ok) {
-            location.reload();
-        } else {
+        if (!response.ok) {
             console.error('Error moving item');
         }
     });
